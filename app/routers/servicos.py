2558b1500903baa57df313c3187c449645361f2b
Willian Assufi <willian_assufialbanez@hotmail.com>
2026-07-23 16:34:46 -0300

    feat: adiciona metricas de servico

--- app/routers/servicos.py
+++ app/routers/servicos.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from sqlalchemy import select
+from sqlalchemy import select, func
 
 from app.database import get_db
-from app.models import Servico
+from app.models import Servico, Verificacao
 from app.schemas import ServicoCreate, ServicoOut, ServicoUpdate
 
 
@@ -66,3 +66,29 @@ def deletar_servico(servico_id: int, db: Session = Depends (get_db)):
     
     db.delete(servico)
     db.commit()
+
+@router.get("/{servico_id}/metricas")
+def obter_metricas(servico_id: int, db: Session = Depends(get_db)):
+    servico = db.get(Servico, servico_id)
+    
+    if servico is None:
+        raise HTTPException(status_code=404, detail="Serviço não encontrado")
+    
+    query_total = select(func.count()).select_from(Verificacao).where(Verificacao.servico_id == servico_id)
+    total = db.execute(query_total).scalar()
+
+    if total == 0:
+        return {"mensagem": "sem verificações ainda", "uptime %": None}
+
+    query_up = select(func.count()).select_from(Verificacao).where(Verificacao.servico_id == servico_id, Verificacao.status == "UP")
+    up = db.execute(query_up).scalar()
+
+    uptime = round((up / total) * 100, 2)
+
+    query_tempo = select(func.avg(Verificacao.tempo_resposta_ms)).select_from(Verificacao).where(Verificacao.servico_id == servico_id)
+    tempo_medio = db.execute(query_tempo).scalar()
+
+    return {"uptime %": uptime,
+            "tempo_medio": round(tempo_medio) if tempo_medio is not None else None}
+
+    
